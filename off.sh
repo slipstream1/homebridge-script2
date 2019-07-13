3d54b769d810796d8a50c4ec505fcf1910d83ece
slipstream1 <slipstream1@users.noreply.github.com>
2019-07-13 17:14:09 +0100

    Update off.sh

--- off.sh
+++ off.sh
@@ -1,3 +1,5 @@
 echo "OFF"
-airctrl 192.168.100.34 --pwr 0
+# replace X.X.X.X with your air purifiers IP address - i.e 192.168.0.5
+# airctrl 192.160.0.5 --pwr 0
+airctrl X.X.X.X --pwr 0
 #rm ~/script.flag
